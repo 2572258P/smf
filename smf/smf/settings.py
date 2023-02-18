a75f1785e80ef32aa75c12b2a1c1d45890ff32e3
2572258P <2572258P@student.gla.ac.uk>
2023-02-18 04:51:22 +0000

    IP host added

--- smf/smf/settings.py
+++ smf/smf/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-6#_f*nd-k&=ovp5v%y)x)0dt4(_1l5zqk&5*m==b966&kuy%s)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.133.142.110','127.0.0.1','35.176.27.58']
+ALLOWED_HOSTS = ['18.133.142.110','127.0.0.1','35.176.27.58','www.uogsmf.org']
 
 
 # Passwords are saved in DB with encryption
